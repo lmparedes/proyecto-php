3c0befdf2b3f2a32667c7daf651b4c90b0a9eb93
Lili Guerrero <lmguerrero1996@gmail.com>
2023-03-07 21:28:09 -0300

    Se agrega la cabecera y el menu de navegacion

---
+++ proyecto.php/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Blog de VideoJuegos</title>
+    </head>
+<body>
+    <!-- CABECERA -->
+    <header>
+        <div id="logo">
+            <a href="index.php">
+                Blog de videojuegos
+            </a>
+        </div>
+    </header>
+    <!-- MENU -->
+    <nav id="nav">
+        <ul>
+            <li>
+                <a href="index.php">inicio</a>
+            </li>
+            <li>
+                <a href="index.php">Categoria 1</a>
+            </li>
+            <li>
+                <a href="index.php">Categoria 2</a>
+            </li>
+            <li>
+                <a href="index.php">Categoria 3</a>
+            </li>
+            <li>
+                <a href="index.php">Categoria 4</a>
+            </li>
+            <li>
+                <a href="index.php">Sobre mi</a>
+            </li>
+            <li>
+                <a href="index.php">Contacto</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- BARRA LATERAL -->
+    <!-- CAJA PRINCIPAL -->
+    <!-- PIE DE PAGINA -->
+</body>
+</html>
