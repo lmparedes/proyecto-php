869527212d91fd77b7334a221456e20573831956
Lili Guerrero <lmguerrero1996@gmail.com>
2023-03-11 22:38:18 -0300

    HTML del contenido pprincipal y del pie de pagina

--- proyecto.php/index.php
+++ proyecto.php/index.php
@@ -6,13 +6,12 @@
     </head>
 <body>
     <!-- CABECERA -->
-    <header>
+    <header id="cabecera">
         <div id="logo">
             <a href="index.php">
                 Blog de videojuegos
             </a>
         </div>
-    </header>
     <!-- MENU -->
     <nav id="nav">
         <ul>
@@ -39,8 +38,67 @@
             </li>
         </ul>
     </nav>
-    <!-- BARRA LATERAL -->
-    <!-- CAJA PRINCIPAL -->
+</header>
+        <div id="contenedor">
+    <!-- BARRA LATERAL -->  
+        <aside id="sidebar">
+            <div id="login" class="bloque">
+                <h3>Identificate</h3>
+                <form action="login.php" method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" />
+
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password" />
+
+                    <input type="submit" value="Entrar" />
+                </form>
+            </div>
+            <div id="register" class="block-aside">
+                <h3>Registrate</h3>
+                <form action="registro.php" method="POST">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre" />
+
+                    <label for="apellidos">Apellidos</label>
+                    <input type="text" name="apellidos" />
+
+                    <input type="submit" value="Registrar" />
+                </form>
+            </div>
+        </aside>
+    <!-- CAJA PRINCIPAL --> 
+    <div id="principal">
+        <h1>Ultimas entradas</h1>
+        <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </article>
+        <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </article>
+             <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </article>
+             <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </article>
+    </div>     
+        </div>
     <!-- PIE DE PAGINA -->
+    <footer id="pie">
+        <p>Desarrollado por Liliana Guerrero copy; 2023</p>
+    </footer>
 </body>
 </html>
