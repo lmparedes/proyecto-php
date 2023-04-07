c174c6df20b10c1bea18cb929c070162a260a08b
Lili Guerrero <lmguerrero1996@gmail.com>
2023-04-07 17:55:16 -0400

    Crear Categorias

--- proyecto.php/includes/conexion.php
+++ proyecto.php/includes/conexion.php
@@ -6,4 +6,9 @@
             $db = mysqli_connect("localhost","root","",'blog');
 
             mysqli_query($db, "SET NAME 'UTF-8'");
+
+//iniciar sesion
+if(!isset($_SESSION)){ 
+session_start();
+}
 ?>
